e5d2a6a41db98e63b3a7b57938fc4e3c959a393a
Paul Beckingham <paul@beckingham.net>
2016-01-26 09:28:43 -0500

    Docs: Comments referenceѕ text.h

--- src/common.h
+++ src/common.h
@@ -32,7 +32,7 @@
 #include <string>
 #include <vector>
 
-// text.cpp, Non-UTF-8 aware.
+// common.cpp, Non-UTF-8 aware.
 void wrapText (std::vector <std::string>&, const std::string&, const int, bool);
 int longestWord (const std::string&);
 int longestLine (const std::string&);
