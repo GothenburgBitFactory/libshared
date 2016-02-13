fafcb6aa3ccd6f09352441e9b709d77d547e2948
Paul Beckingham <paul@beckingham.net>
2016-02-13 12:09:15 -0500

    Datetime: Added ::operator>=

--- src/Datetime.h
+++ src/Datetime.h
@@ -61,6 +61,7 @@ public:
   bool operator<  (const Datetime&) const;
   bool operator>  (const Datetime&) const;
   bool operator<= (const Datetime&) const;
+  bool operator>= (const Datetime&) const;
 
 private:
   void clear ();
