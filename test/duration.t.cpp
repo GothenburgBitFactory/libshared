3425510eee40c4e524cd732a1137d33f10340334
Paul Beckingham <paul@beckingham.net>
2016-02-16 09:15:59 -0500

    Tests: Added designated form tests

--- test/duration.t.cpp
+++ test/duration.t.cpp
@@ -64,7 +64,7 @@ void testParse (
 ////////////////////////////////////////////////////////////////////////////////
 int main (int, char**)
 {
-  UnitTest t (12);
+  UnitTest t (221);
 
   Duration dur;
   std::string::size_type start = 0;
@@ -85,6 +85,34 @@ int main (int, char**)
 
   t.notok (dur.parse ("PT1", start), "PT1 --> false");
   t.is ((int)start, 0,               "PT1[0]");
+
+  int year  = 365 * 86400;
+  int month =  30 * 86400;
+  int day   =       86400;
+  int h     =        3600;
+  int m     =          60;
+
+  // Designated.
+  //            input              i  Year  Mo   Da  Ho  Mi         Se                           time_t           format     vague
+  testParse (t, "P1Y",             3,    1,  0,   0,  0,  0,         0,                            year,          "P365D",   "1.0y");
+  testParse (t, "P1M",             3,    0,  1,   0,  0,  0,         0,                           month,           "P30D",     "4w");
+  testParse (t, "P1D",             3,    0,  0,   1,  0,  0,         0,                             day,            "P1D",     "1d");
+  testParse (t, "P1Y1M",           5,    1,  1,   0,  0,  0,         0,                    year + month,          "P395D",   "1.1y");
+  testParse (t, "P1Y1D",           5,    1,  0,   1,  0,  0,         0,                      year + day,          "P366D",   "1.0y");
+  testParse (t, "P1M1D",           5,    0,  1,   1,  0,  0,         0,                     month + day,           "P31D",     "4w");
+  testParse (t, "P1Y1M1D",         7,    1,  1,   1,  0,  0,         0,              year + month + day,          "P396D",   "1.1y");
+  testParse (t, "PT1H",            4,    0,  0,   0,  1,  0,         0,                               h,           "PT1H",     "1h");
+  testParse (t, "PT1M",            4,    0,  0,   0,  0,  1,         0,                               m,           "PT1M",   "1min");
+  testParse (t, "PT1S",            4,    0,  0,   0,  0,  0,         1,                               1,           "PT1S",     "1s");
+  testParse (t, "PT1H1M",          6,    0,  0,   0,  1,  1,         0,                           h + m,         "PT1H1M",     "1h");
+  testParse (t, "PT1H1S",          6,    0,  0,   0,  1,  0,         1,                           h + 1,         "PT1H1S",     "1h");
+  testParse (t, "PT1M1S",          6,    0,  0,   0,  0,  1,         1,                           m + 1,         "PT1M1S",   "1min");
+  testParse (t, "PT1H1M1S",        8,    0,  0,   0,  1,  1,         1,                       h + m + 1,       "PT1H1M1S",     "1h");
+  testParse (t, "P1Y1M1DT1H1M1S", 14,    1,  1,   1,  1,  1,         1,  year + month + day + h + m + 1,   "P396DT1H1M1S",   "1.1y");
+  testParse (t, "PT24H",           5,    0,  0,   0, 24,  0,         0,                             day,            "P1D",     "1d");
+  testParse (t, "PT40000000S",    11,    0,  0,   0,  0,  0,  40000000,                        40000000, "P462DT23H6M40S",   "1.3y");
+  testParse (t, "PT3600S",         7,    0,  0,   0,  0,  0,      3600,                               h,           "PT1H",     "1h");
+  testParse (t, "PT60M",           5,    0,  0,   0,  0, 60,         0,                               h,           "PT1H",     "1h");
 
   return 0;
 }
