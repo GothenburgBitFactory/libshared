59e2006833464d8fb1a3d8e2f8d9c5d8040430c7
Paul Beckingham <paul@beckingham.net>
2016-07-18 16:34:52 -0400

    Datetime: Fixed eow and eoww errors

--- src/Datetime.cpp
+++ src/Datetime.cpp
@@ -1654,14 +1654,17 @@ bool Datetime::initializeEow (const std::string& token)
   {
     time_t now = time (nullptr);
     struct tm* t = localtime (&now);
+
+    if (Datetime::lookForwards)
+      t->tm_mday += 8 - t->tm_wday;
+    else
+      t->tm_mday -= t->tm_wday - 1;
+
     t->tm_hour = t->tm_min = t->tm_sec = 0;
-
-    int extra = (8 - t->tm_wday) % 7;
-    t->tm_mday += extra;
-
     t->tm_isdst = -1;
     _date = mktime (t);
     return true;
+
   }
 
   return false;
