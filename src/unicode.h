1541cb2df762da08ac64b262d9152d1234eb76fb
Paul Beckingham <paul@beckingham.net>
2016-12-22 12:48:26 -0500

    unicode: Added unicodeAlpha

--- src/unicode.h
+++ src/unicode.h
@@ -31,6 +31,7 @@ bool unicodeWhitespace           (unsigned int);
 bool unicodeHorizontalWhitespace (unsigned int);
 bool unicodeVerticalWhitespace   (unsigned int);
 bool unicodePunctuation          (unsigned int);
+bool unicodeAlpha                (unsigned int);
 bool unicodeLatinAlpha           (unsigned int);
 bool unicodeLatinDigit           (unsigned int);
 bool unicodeHexDigit             (unsigned int);
