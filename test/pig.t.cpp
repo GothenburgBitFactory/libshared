8851eafcf88c24e0cc2f608beb955e0e892d6262
Paul Beckingham <paul@beckingham.net>
2016-12-24 11:39:02 -0500

    Tests: Updated Pig::dump tests

--- test/pig.t.cpp
+++ test/pig.t.cpp
@@ -38,127 +38,127 @@ int main (int, char**)
   Pig p0 ("12345");
   t.notok (p0.skip ('0'),   "skip='0', '12345' --> false");
   t.ok (p0.skip ('1'),      "skip='1', '12345' --> true");
-  t.is (p0.dump (),         "≪12345≫ l5 c1", "dump: " + p0.dump ());
+  t.ok (p0.dump ().find (" 1/5") != std::string::npos, "dump: " + p0.dump ());
 
   t.ok (p0.skipN (3),       "skipN=3 '12345' --> true");
-  t.is (p0.dump (),         "≪12345≫ l5 c4", "dump: " + p0.dump ());
+  t.ok (p0.dump ().find (" 4/5") != std::string::npos, "dump: " + p0.dump ());
 
   t.notok (p0.skipN (2),    "skipN=2 '5' --> false");
-  t.is (p0.dump (),         "≪12345≫ l5 c4", "dump: " + p0.dump ());
+  t.ok (p0.dump ().find (" 4/5") != std::string::npos, "dump: " + p0.dump ());
 
   // Pig::skipWS
   Pig p1 ("  one");
   t.ok (p1.skipWS (),       "skipWS '  one' --> true");
-  t.is (p1.dump (),         "≪  one≫ l5 c2", "dump: " + p1.dump ());
+  t.ok (p1.dump ().find (" 2/5") != std::string::npos, "dump: " + p1.dump ());
 
   t.notok (p1.skipWS (),    "skipWS 'one' --> false");
-  t.is (p1.dump (),         "≪  one≫ l5 c2", "dump: " + p1.dump ());
+  t.ok (p1.dump ().find (" 2/5") != std::string::npos, "dump: " + p1.dump ());
 
   // Pig::skipLiteral
   Pig p2 ("onetwo");
   t.notok (p2.skipLiteral ("two"), "skipLiteral=two 'onetwo' --> false");
   t.ok (p2.skipLiteral ("one"),    "skipLiteral=one 'onetwo' --> true");
-  t.is (p2.dump (),                "≪onetwo≫ l6 c3", "dump: " + p2.dump ());
+  t.ok (p2.dump ().find (" 3/6") != std::string::npos, "dump: " + p2.dump ());
 
   // Pig::getUntilWS
   Pig p3 ("one two three  ");
   std::string value;
   t.ok (p3.getUntilWS (value), "getUntilWS 'one two three  ' --> true");
   t.is (value, "one",          "getUntilWS 'one two three  ' --> 'one'");
-  t.is (p3.dump (),            "≪one two three  ≫ l15 c3", "dump: " + p3.dump ());
+  t.ok (p3.dump ().find (" 3/15") != std::string::npos, "dump: " + p3.dump ());
 
   t.ok (p3.skipWS (),          "skipWS ' two three  ' --> true");
 
   t.ok (p3.getUntilWS (value), "getUntilWS 'two three  ' --> true");
   t.is (value, "two",          "getUntilWS 'two three  ' --> 'two'");
-  t.is (p3.dump (),            "≪one two three  ≫ l15 c7", "dump: " + p3.dump ());
+  t.ok (p3.dump ().find (" 7/15") != std::string::npos, "dump: " + p3.dump ());
 
   t.ok (p3.skipWS (),          "skipWS ' three  ' --> true");
 
   t.ok (p3.getUntilWS (value), "getUntilWS 'three  ' --> true");
   t.is (value, "three",        "getUntilWS 'three  ' --> 'three'");
-  t.is (p3.dump (),            "≪one two three  ≫ l15 c13", "dump: " + p3.dump ());
+  t.ok (p3.dump ().find (" 13/15") != std::string::npos, "dump: " + p3.dump ());
 
   Pig p3a ("9th ");
   t.ok (p3a.getUntilWS (value), "getUntilWS '9th ' --> true");
   t.is (value, "9th",           "getUntilWS '9th ' --> '9th'");
-  t.is (p3a.dump (),            "≪9th ≫ l4 c3", "dump: " + p3a.dump ());
+  t.ok (p3a.dump ().find (" 3/4") != std::string::npos, "dump: " + p3a.dump ());
 
   // Pig::getDigit
   Pig p4 (" 123");
   int n;
   t.notok (p4.getDigit (n), "getDigit ' 123' --> false");
   t.ok (p4.skipWS (),       "skipWS ' 123' --> true");
-  t.is (p4.dump (),         "≪ 123≫ l4 c1", "dump: " + p4.dump ());
+  t.ok (p4.dump ().find (" 1/4") != std::string::npos, "dump: " + p4.dump ());
   t.ok (p4.getDigit (n),    "getDigit '123' --> true");
   t.is (n, 1,               "getDigit '123' --> '1'");
-  t.is (p4.dump (),         "≪ 123≫ l4 c2", "dump: " + p4.dump ());
+  t.ok (p4.dump ().find (" 2/4") != std::string::npos, "dump: " + p4.dump ());
 
   // Pig::getDigits
   Pig p5 ("123 ");
   t.ok (p5.getDigits (n),   "getDigits '123 ' --> true");
   t.is (n, 123,             "getDigits '123 ' --> 123");
-  t.is (p5.dump (),         "≪123 ≫ l4 c3", "dump: " + p5.dump ());
+  t.ok (p5.dump ().find (" 3/4") != std::string::npos, "dump: " + p5.dump ());
 
   Pig p6 ("1");
   t.notok (p6.eos (),       "eos '1' --> false");
   t.ok (p6.getDigit (n),    "getDigit '1' --> true");
   t.notok (p6.getDigit (n), "getDigit '' --> false");
   t.ok (p6.eos (),          "eos '' --> true");
-  t.is (p6.dump (),         "≪1≫ l1 c1", "dump: " + p6.dump ());
+  t.ok (p6.dump ().find (" 1/1") != std::string::npos, "dump: " + p6.dump ());
 
   // Pig::getNumber
   Pig p7 ("1 ");
   t.ok (p7.getNumber (value), "getNumber '1 ' --> true");
   t.is (value, "1",           "getNumber '1 ' --> '1'");
-  t.is (p7.dump (),           "≪1 ≫ l2 c1", "dump: " + p7.dump ());
+  t.ok (p7.dump ().find (" 1/2") != std::string::npos, "dump: " + p7.dump ());
 
   Pig p8 ("3.14");
   t.ok (p8.getNumber (value), "getNumber '3.14' --> true");
   t.is (value, "3.14",        "getNumber '3.14' --> '3.14'");
-  t.is (p8.dump (),           "≪3.14≫ l4 c4", "dump: " + p8.dump ());
+  t.ok (p8.dump ().find (" 4/4") != std::string::npos, "dump: " + p8.dump ());
 
   Pig p9 ("1.23e-4 ");
   t.ok (p9.getNumber (value), "getNumber '1.23e-4 ' --> true");
   t.is (value, "1.23e-4",     "getNumber '1.23e-4 ' --> '1.23e-4'");
-  t.is (p9.dump (),           "≪1.23e-4 ≫ l8 c7", "dump: " + p9.dump ());
+  t.ok (p9.dump ().find (" 7/8") != std::string::npos, "dump: " + p9.dump ());
 
   Pig p10 ("2.34e-5");
   double dvalue;
   t.ok (p10.getNumber (dvalue), "getNumber '2.34e-5' --> true");
   t.is (dvalue, 2.34e-5, 1e-6,  "getNumber '2.34e-5' --> 2.34e-5 +/- 1e-6");
-  t.is (p10.dump (),            "≪2.34e-5≫ l7 c7", "dump: " + p10.dump ());
+  t.ok (p10.dump ().find (" 7/7") != std::string::npos, "dump: " + p10.dump ());
 
   // Pig::getRemainder
   Pig p11 ("123");
   t.ok (p11.skipN (1),       "skipN=1 '123' --> true");
-  t.is (p11.dump (),         "≪123≫ l3 c1", "dump: " + p11.dump ());
+  t.ok (p11.dump ().find (" 1/3") != std::string::npos, "dump: " + p11.dump ());
 
   t.ok (p11.getRemainder (value), "getRemainder '23' --> true");
   t.is (value, "23",        "getRemainder '23' --> '23'");
-  t.is (p11.dump (),         "≪123≫ l3 c3", "dump: " + p11.dump ());
+  t.ok (p11.dump ().find (" 3/3") != std::string::npos, "dump: " + p11.dump ());
 
   t.notok (p11.getRemainder (value), "getRemainder '' --> false");
-  t.is (p11.dump (),         "≪123≫ l3 c3", "dump: " + p11.dump ());
+  t.ok (p11.dump ().find (" 3/3") != std::string::npos, "dump: " + p11.dump ());
 
   // Pig::peek
   Pig p12 ("123");
   t.is (p12.peek (), '1',     "peek '123' --> '1'");
-  t.is (p12.dump (),          "≪123≫ l3 c0", "dump: " + p12.dump ());
+  t.ok (p12.dump ().find (" 0/3") != std::string::npos, "dump: " + p12.dump ());
   t.is (p12.peek (2), "12",   "peek=2 '123' --> '12'");
   t.is (p12.peek (3), "123",  "peek=3 '123' --> '123'");
   t.is (p12.peek (4), "123",  "peek=4 '123' --> '123'");
   t.is (p12.peek (20), "123", "peek=20 '123' --> '123'");
-  t.is (p12.dump (),          "≪123≫ l3 c0", "dump: " + p12.dump ());
+  t.ok (p12.dump ().find (" 0/3") != std::string::npos, "dump: " + p12.dump ());
 
   // Pig::save, Pig::restore
   Pig p13 ("123");
   t.is ((int)p13.save (), 0,    "save '123' --> 0");
   t.ok (p13.skipN (2),          "skipN=2 '123' --> true");
-  t.is (p13.dump (),            "≪123≫ l3 c2", "dump: " + p13.dump ());
+  t.ok (p13.dump ().find (" 2/3") != std::string::npos, "dump: " + p13.dump ());
   t.is ((int)p13.cursor (), 2,  "cursor '123' --> 2");
   t.is ((int)p13.restore (), 0, "restore '123' --> 0");
-  t.is (p13.dump (),            "≪123≫ l3 c0", "dump: " + p13.dump ());
+  t.ok (p13.dump ().find (" 0/3") != std::string::npos, "dump: " + p13.dump ());
 
   // Pig::getOneOf
   Pig p14 ("fourteenfour five");
@@ -166,26 +166,26 @@ int main (int, char**)
   t.is (value, "fourteen",                          "getOneOf={fourteen,four} 'fourteenfour five' --> 'fourteen'");
   t.ok (p14.getOneOf ({"fourteen", "four"}, value), "getOneOf={fourteen,four} 'fourteenfour five' --> true");
   t.is (value, "four",                              "getOneOf={fourteen,four} 'fourteenfour five' --> 'four'");
-  t.is (p14.dump (),                                "≪fourteenfour five≫ l17 c12", "dump: " + p14.dump ());
+  t.ok (p14.dump ().find (" 12/17") != std::string::npos, "dump: " + p14.dump ());
   t.notok (p14.getOneOf ({"five"}, value),          "getOneOf={five} 'fourteenfour five' --> false");
-  t.is (p14.dump (),                                "≪fourteenfour five≫ l17 c12", "dump: " + p14.dump ());
+  t.ok (p14.dump ().find (" 12/17") != std::string::npos, "dump: " + p14.dump ());
 
   // Pig::getHexDigit
   Pig p15 (" 9aF");
   t.notok (p15.getHexDigit (n), "getHexDigit ' 9aF' --> false");
   t.ok (p15.skipWS (),          "skipWS ' 9aF' --> true");
-  t.is (p15.dump (),            "≪ 9aF≫ l4 c1", "dump: " + p15.dump ());
+  t.ok (p15.dump ().find (" 1/4") != std::string::npos, "dump: " + p15.dump ());
   t.ok (p15.getHexDigit (n),    "getHexDigit '9aF' --> true");
   t.is (n, 9,                   "getHexDigit '9aF' --> '9'");
-  t.is (p15.dump (),            "≪ 9aF≫ l4 c2", "dump: " + p15.dump ());
+  t.ok (p15.dump ().find (" 2/4") != std::string::npos, "dump: " + p15.dump ());
 
   t.ok (p15.getHexDigit (n),    "getHexDigit '9aF' --> true");
   t.is (n, 10,                  "getHexDigit '9aF' --> '10'");
-  t.is (p15.dump (),            "≪ 9aF≫ l4 c3", "dump: " + p15.dump ());
+  t.ok (p15.dump ().find (" 3/4") != std::string::npos, "dump: " + p15.dump ());
 
   t.ok (p15.getHexDigit (n),    "getHexDigit '9aF' --> true");
   t.is (n, 15,                  "getHexDigit '9aF' --> '15'");
-  t.is (p15.dump (),            "≪ 9aF≫ l4 c4", "dump: " + p15.dump ());
+  t.ok (p15.dump ().find (" 4/4") != std::string::npos, "dump: " + p15.dump ());
 
   // Pig::getQuoted
   Pig p16 ("");
@@ -226,23 +226,23 @@ int main (int, char**)
   Pig p24 ("122333444455555555");
   t.ok (p24.getDigit (n),   "getDigit   '122333444455555555' --> true");
   t.is (n, 1,               "getDigit   '122333444455555555' --> 1");
-  t.is (p24.dump (),        "≪122333444455555555≫ l18 c1", "dump: " + p24.dump ());
+  t.ok (p24.dump ().find (" 1/18") != std::string::npos, "dump: " + p24.dump ());
 
   t.ok (p24.getDigit2 (n),  "getDigit2   '22333444455555555' --> true");
   t.is (n, 22,              "getDigit2   '22333444455555555' --> 22");
-  t.is (p24.dump (),        "≪122333444455555555≫ l18 c3", "dump: " + p24.dump ());
+  t.ok (p24.dump ().find (" 3/18") != std::string::npos, "dump: " + p24.dump ());
 
   t.ok (p24.getDigit3 (n),  "getDigit3   '22333444455555555' --> true");
   t.is (n, 333,             "getDigit3   '22333444455555555' --> 333");
-  t.is (p24.dump (),        "≪122333444455555555≫ l18 c6", "dump: " + p24.dump ());
+  t.ok (p24.dump ().find (" 6/18") != std::string::npos, "dump: " + p24.dump ());
 
   t.ok (p24.getDigit4 (n),  "getDigit4   '22333444455555555' --> true");
   t.is (n, 4444,            "getDigit4   '22333444455555555' --> 4444");
-  t.is (p24.dump (),        "≪122333444455555555≫ l18 c10", "dump: " + p24.dump ());
+  t.ok (p24.dump ().find (" 10/18") != std::string::npos, "dump: " + p24.dump ());
 
   t.ok (p24.getDigits (n),  "getDigits   '22333444455555555' --> true");
   t.is (n, 55555555,        "getDigits   '22333444455555555' --> 55555555");
-  t.is (p24.dump (),        "≪122333444455555555≫ l18 c18", "dump: " + p24.dump ());
+  t.ok (p24.dump ().find (" 18/18") != std::string::npos, "dump: " + p24.dump ());
 
   t.ok (p24.eos (),         "eos --> true");
 
@@ -250,19 +250,19 @@ int main (int, char**)
   Pig p25 ("one two three");
   t.ok (p25.getUntil (' ', value), "getUntil ' ', 'one two three' --> true");
   t.is (value, "one",              "getUntil ' ', 'one two three' --> 'one'");
-  t.is (p25.dump (),               "≪one two three≫ l13 c3", "dump: " + p3.dump ());
+  t.ok (p25.dump ().find (" 3/13") != std::string::npos, "dump: " + p25.dump ());
 
   t.ok (p25.skipWS (),             "skipWS ' two three' --> true");
 
   t.ok (p25.getUntil (' ', value), "getUntil ' ', 'two three' --> true");
   t.is (value, "two",              "getUntil ' ', 'two three' --> 'two'");
-  t.is (p25.dump (),               "≪one two three≫ l13 c7", "dump: " + p3.dump ());
+  t.ok (p25.dump ().find (" 7/13") != std::string::npos, "dump: " + p25.dump ());
 
   t.ok (p25.skipWS (),             "skipWS ' three' --> true");
 
   t.ok (p25.getUntil (' ', value), "getUntil ' ', 'three' --> true");
   t.is (value, "three",            "getUntil ' ', 'three' --> 'three'");
-  t.is (p25.dump (),               "≪one two three≫ l13 c13", "dump: " + p3.dump ());
+  t.ok (p25.dump ().find (" 13/13") != std::string::npos, "dump: " + p25.dump ());
 
   Pig p25a ("[foo]");
   t.ok (p25a.skip ('['),            "Pig::skip [, [foo] --> foo] --> true");
@@ -281,12 +281,12 @@ int main (int, char**)
   Pig p27 ("1 ");
   t.ok (p27.getDecimal (value), "getDecimal '1 ' --> true");
   t.is (value, "1",             "getDecimal '1 ' --> '1'");
-  t.is (p27.dump (),            "≪1 ≫ l2 c1", "dump: " + p27.dump ());
+  t.ok (p27.dump ().find (" 1/2") != std::string::npos, "dump: " + p27.dump ());
 
   Pig p28 ("3.14");
   t.ok (p28.getDecimal (value), "getDecimal '3.14' --> true");
   t.is (value, "3.14",          "getDecimal '3.14' --> '3.14'");
-  t.is (p28.dump (),            "≪3.14≫ l4 c4", "dump: " + p28.dump ());
+  t.ok (p28.dump ().find (" 4/4") != std::string::npos, "dump: " + p28.dump ());
 
   Pig p29 ("1:23:45");
   int h, m, s;
@@ -317,13 +317,13 @@ int main (int, char**)
   Pig p32 ("abc");
   t.ok (p32.getCharacter (n), "getcharacter 'abc' --> true");
   t.is (n, 97,                "getcharacter 'abc ' --> 'a'");
-  t.is (p32.dump (),          "≪abc≫ l3 c1", "dump: " + p32.dump ());
+  t.ok (p32.dump ().find (" 1/3") != std::string::npos, "dump: " + p32.dump ());
   t.ok (p32.getCharacter (n), "getcharacter 'abc' --> true");
   t.is (n, 98,                "getcharacter 'abc ' --> 'b'");
-  t.is (p32.dump (),          "≪abc≫ l3 c2", "dump: " + p32.dump ());
+  t.ok (p32.dump ().find (" 2/3") != std::string::npos, "dump: " + p32.dump ());
   t.ok (p32.getCharacter (n), "getcharacter 'abc' --> true");
   t.is (n, 99,                "getcharacter 'abc ' --> 'c'");
-  t.is (p32.dump (),          "≪abc≫ l3 c3", "dump: " + p32.dump ());
+  t.ok (p32.dump ().find (" 3/3") != std::string::npos, "dump: " + p32.dump ());
   t.ok (p32.eos (),           "eos --> true");
 
   return 0;
