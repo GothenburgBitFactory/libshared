d03b0a5dd1ce4193bda90dbc59c9b57b45616235
Paul Beckingham <paul@beckingham.net>
2017-03-06 09:22:04 -0500

    Tests: Added 'socy'

--- test/dates.t.cpp
+++ test/dates.t.cpp
@@ -52,7 +52,7 @@ void testInit (UnitTest& t, const std::string& value, Datetime& var)
 ////////////////////////////////////////////////////////////////////////////////
 int main (int, char**)
 {
-  UnitTest t (166);
+  UnitTest t (170);
 
   Datetime sunday;    testInit (t, "sunday",    sunday);
   Datetime monday;    testInit (t, "monday",    monday);
@@ -166,6 +166,7 @@ int main (int, char**)
   Datetime eonq;           testInit (t, "eonq", eonq);
   Datetime eoq;            testInit (t, "eoq", eoq);
   Datetime sopy;           testInit (t, "sopy", sopy);
+  Datetime socy;           testInit (t, "socy", socy);
 
   Datetime soy;            testInit (t, "soy", soy);
   Datetime eoy;            testInit (t, "eoy", eoy);
@@ -225,6 +226,7 @@ int main (int, char**)
   t.ok (eonq > sonq,                "eonq > sonq");
   t.ok (eoq == eocq,                "eoq == eocq");
   t.ok (sopy < soy,                 "sopy < soy");
+  t.ok (socy > sopy,                "socy > sopy");
 
   t.ok (yesterday < today,          "yesterday < today");
   t.ok (today < tomorrow,           "today < tomorrow");
