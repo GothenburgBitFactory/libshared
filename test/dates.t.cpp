ebba9a90c51ed2347f51654370cf8ad57478fd0e
Paul Beckingham <paul@beckingham.net>
2017-03-06 08:18:26 -0500

    Tests: Added 'eopm'

--- test/dates.t.cpp
+++ test/dates.t.cpp
@@ -52,7 +52,7 @@ void testInit (UnitTest& t, const std::string& value, Datetime& var)
 ////////////////////////////////////////////////////////////////////////////////
 int main (int, char**)
 {
-  UnitTest t (143);
+  UnitTest t (145);
 
   Datetime sunday;    testInit (t, "sunday",    sunday);
   Datetime monday;    testInit (t, "monday",    monday);
@@ -153,6 +153,7 @@ int main (int, char**)
   Datetime socm;           testInit (t, "socm", socm);
   Datetime sonm;           testInit (t, "sonm", sonm);
   Datetime som;            testInit (t, "som", som);
+  Datetime eopm;           testInit (t, "eopm", eopm);
 
   Datetime soy;            testInit (t, "soy", soy);
   Datetime eoy;            testInit (t, "eoy", eoy);
@@ -202,6 +203,7 @@ int main (int, char**)
   t.ok (sopm < som,                 "sopm < som");
   t.ok (socm < eocm,                "socm < eocm");
   t.ok (eocm == sonm,               "eocm == sonm");
+  t.ok (eopm == socm,               "eopm == socm");
 
   t.ok (yesterday < today,          "yesterday < today");
   t.ok (today < tomorrow,           "today < tomorrow");
