e9aca8dbbf2c6d937e16d59b6612e04cbbc8e320
Rosen Penev <rosenp@gmail.com>
2021-04-24 15:15:58 -0700

    clang-tidy: avoid double const
    Found with readability-const-return-type
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- src/Lexer.h
+++ src/Lexer.h
@@ -54,7 +54,7 @@ public:
 
   // Static helpers.
   static std::vector <std::tuple <std::string, Lexer::Type>> tokenize (const std::string&);
-  static const std::string typeName          (const Lexer::Type&);
+  static std::string typeName          (const Lexer::Type&);
   static bool isSingleCharOperator           (int);
   static bool isDoubleCharOperator           (int, int, int);
   static bool isTripleCharOperator           (int, int, int, int);
